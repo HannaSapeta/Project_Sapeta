30fa0796415eaaa87510d1115b155766e8b5c47a
HannaSapeta <Hanna.sapieta@cs.khpi.edu.ua>
2026-05-30 22:44:55 +0300

    8Лб

---
+++ Tests/Test1.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using WpfUI.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class PhotoTests
+    {
+        [TestMethod]
+        public void CreatePhotoTest()
+        {
+            Photo photo = new Photo(
+                "photo.jpg",
+                5,
+                "1920x1080");
+
+            Assert.AreEqual(
+                "photo.jpg",
+                photo.FileName);
+        }
+
+        [TestMethod]
+        public void AddPhotoTest()
+        {
+            MainViewModel vm = new MainViewModel();
+
+            int count = vm.Photos.Count;
+
+            vm.AddCommand.Execute(null);
+
+            Assert.AreEqual(
+                count + 1,
+                vm.Photos.Count);
+        }
+
+        [TestMethod]
+        public void DeletePhotoTest()
+        {
+            MainViewModel vm = new MainViewModel();
+
+            Photo photo = new Photo(
+                "1.jpg",
+                2,
+                "800x600");
+
+            vm.Photos.Add(photo);
+
+            vm.SelectedPhoto = photo;
+
+            vm.DeleteCommand.Execute(null);
+
+            Assert.AreEqual(
+                0,
+                vm.Photos.Count);
+        }
+
+        [TestMethod]
+        public void LinqFilterTest()
+        {
+            List<Photo> photos = new List<Photo>
+            {
+                new Photo("a", 1, "1"),
+                new Photo("b", 10, "1"),
+                new Photo("c", 15, "1")
+            };
+
+            var result = photos
+                .Where(x => x.FileSizeMb > 5)
+                .ToList();
+
+            Assert.AreEqual(
+                2,
+                result.Count);
+        }
+
+        [TestMethod]
+        public void PhotoNameValidation()
+        {
+            Photo photo = new Photo();
+
+            photo.FileName = "";
+
+            Assert.IsTrue(
+                string.IsNullOrWhiteSpace(
+                    photo.FileName));
+        }
+    }
+}
---
+++ WpfUI/ViewModels.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Core;
+
+namespace WpfUI.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute();
+        }
+    }
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Photo? selectedPhoto;
+
+        public ObservableCollection<Photo> Photos
+        {
+            get;
+            set;
+        }
+
+        public Photo? SelectedPhoto
+        {
+            get => selectedPhoto;
+            set
+            {
+                selectedPhoto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        public ICommand DeleteCommand { get; }
+
+        public MainViewModel()
+        {
+            Photos = new ObservableCollection<Photo>();
+
+            AddCommand =
+                new RelayCommand(AddPhoto);
+
+            DeleteCommand =
+                new RelayCommand(DeletePhoto);
+        }
+
+        private int counter = 1;
+
+        private void AddPhoto()
+        {
+            Photos.Add(
+                new Photo(
+                    $"Photo_{counter++}.jpg",
+                    5,
+                    "1920x1080"));
+        }
+
+        private void DeletePhoto()
+        {
+            if (SelectedPhoto != null)
+                Photos.Remove(SelectedPhoto);
+        }
+
+        public event PropertyChangedEventHandler?
+            PropertyChanged;
+
+        protected void OnPropertyChanged(
+            [CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(property));
+        }
+    }
+}
